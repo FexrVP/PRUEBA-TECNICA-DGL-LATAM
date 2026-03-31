95485855983706c40f0174fc63ebe674ae6d5ed3
Diego Fernando Vazquez Perez <vazquez.diego.3007@gmail.com>
2026-03-31 16:42:20 -0600

    chore: Estructura inicial del proyecto

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductList } from './components/product-list/product-list';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule, CommonModule, ProductList],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('frontend');
+  
+}
